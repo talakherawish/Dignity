78b973a06ef93c384049a2b328d70d570265dbf5
Tala Kherawish <talakherawish@gmail.com>
2026-07-18 14:22:11 +0300

    Simplify GET route response to 'ok'

--- dignity-backend/src/app/my-route/route.ts
+++ dignity-backend/src/app/my-route/route.ts
@@ -1,12 +1,5 @@
-import configPromise from '@payload-config'
-import { getPayload } from 'payload'
-
-export const GET = async (request: Request) => {
-  const payload = await getPayload({
-    config: configPromise,
-  })
-
+export const GET = async () => {
   return Response.json({
-    message: 'This is an example of a custom route.',
+    message: 'ok',
   })
 }
