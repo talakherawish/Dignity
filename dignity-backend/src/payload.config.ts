304fef2f5c6dd8c56c34e871bbc3f5f3226bd3c7
Tala Kherawish <talakherawish@gmail.com>
2026-07-19 20:33:42 +0300

    Add files via upload

--- dignity-backend/src/payload.config.ts
+++ dignity-backend/src/payload.config.ts
@@ -1,5 +1,6 @@
 import { mongooseAdapter } from '@payloadcms/db-mongodb'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
 import path from 'path'
 import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
@@ -44,5 +45,13 @@ export default buildConfig({
     url: process.env.DATABASE_URL || '',
   }),
   sharp,
-  plugins: [],
+  plugins: [
+    vercelBlobStorage({
+      collections: {
+        media: true,
+      },
+      token: process.env.BLOB_READ_WRITE_TOKEN || '',
+      clientUploads: true,
+    }),
+  ],
 })
